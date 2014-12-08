8e24958329c5bbe9daf6e9f159183696b582186f
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-07 23:27:41 -0600

    Some template refactor and styles

--- app/controllers/tags/index.js
+++ app/controllers/tags/index.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  isLoaded: false,
-  init: function() {
-    console.log('init tags');
-    this.store.find('tag');
-  }
 });
