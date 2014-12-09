b94da5d3e0ba37ce262e830453e39f7508affbdf
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-09 00:05:14 -0600

    Simple filtering working

--- app/controllers/tags/index.js
+++ app/controllers/tags/index.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  // itemController: 'tag',
-  // filteredTags: function() {
-  //   return this.filterBy('name', 'web');
-  // }
+  search: '',
+  filteredTags: function() {
+    var search = this.get('search');
+    if(search.length) {
+      return this.filter(function(tag) {
+        var re = new RegExp(search);
+        return re.test(tag.get('name'));
+      });
+    } else {
+      return this;
+    }
+  }.property('search')
 });
