9cfb420b012db8975a7a53ce0f24e94d4d9d6f17
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-17 12:57:39 -0600

    Random link working across links index

--- app/controllers/links/index.js
+++ app/controllers/links/index.js
@@ -3,18 +3,15 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   total: Ember.computed.alias('this.length'),
   showingRandomLink: false,
-  randomLink: function() {
-    // return this.store.find('link', { random: true })
-  },
+  randomLink: false,
   actions: {
     showRandomLink: function() {
-      // this.set('showingRandomLink', true);
+      var _this = this;
       var random_index = Math.floor(Math.random() * this.get('length'));
-      // console.log( this.get('array[random_index]') );
-      // console.log(this.get(random_index));
       return this.find(function(item, index, enumberable) {
         if (index === random_index) {
-          console.log(item);
+          // console.log(item, _this);
+          _this.set('randomLink', item);
           return true;
         }
       });
